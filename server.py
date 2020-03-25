123fd33e4bbd9646f8328d2e1572364295d00574
victor <vneznanow@yandex.ru>
2020-03-25 21:18:18 +0300

    Автоматический ответ

--- server.py
+++ server.py
@@ -1,7 +1,41 @@
 from flask import Flask, url_for, request, render_template
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, SelectField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "yandexlyceum_secret_key"
+
+prfs = [
+    ('e1', "инженер-исследователь"),
+    ('e2', "пилот"),
+    ('e3', "строитель"),
+    ('e4', "экзобиолог"),
+    ('e5', "врач"),
+    ('e6', "инженер по терраформированию"),
+    ('e7', "климатолог"),
+    ('e8', "специалист по радиационной защите"),
+    ('e9', "астрогеолог"),
+    ('e10', "гляциолог"),
+    ('e11', "инженер жизнеобеспечения"),
+    ('e12', "метеоролог"),
+    ('e13', "оператор марсохода")
+]
+
+
+class AstronautForm(FlaskForm):
+    surname = StringField("Фамилия", validators=[DataRequired()])
+    name = StringField("Имя", validators=[DataRequired()])
+    education = SelectField("Выберите образование",
+                            choices=[('e1', 'Начальное'), ('e2', 'Среднее'), ('e2', 'Высшее')])
+    profession = RadioField("Выберите профессию", choices=prfs)
+    sex = RadioField("Выбретие пол", choices=[('male', 'Мужской'), ('female', 'Женский')])
+    motivation = TextAreaField("Комментарий")
+    ready = BooleanField("Готовы остаться на Марсе?")
+    submit = SubmitField("Отправить")
 
 
 @app.route("/<title_value>")
@@ -41,6 +75,35 @@ def list_prof(ltype):
     ]
 
     return render_template('list_prof.html', title=ltype, ltype=ltype, profs=profs)
+
+
+@app.route("/answer", methods=['POST', 'GET'])
+@app.route("/auto_answer", methods=['POST', 'GET'])
+def auto_answer():
+    form = AstronautForm()
+    if form.validate_on_submit():
+        education = form.education.data
+        if education == "e1":
+            education = "Начальное"
+        elif education == "e2":
+            education = "Среднее"
+        elif education == "e3":
+            education = "Высшее"
+        d = {
+            "title": "Анкета",
+            "surname": form.surname.data,
+            "name": form.name.data,
+            "education": education,
+            "profession": [prof[1] for prof in prfs if prof[0] == form.profession.data][0],
+            "sex": form.sex.data,
+            "motivation": form.motivation.data,
+            "ready": form.ready.data
+        }
+        return render_template("auto_answer.html", title="Ответ",
+                               spec_link=url_for('static', filename="css/form2_style.css"), form=d)
+    else:
+        return render_template("form_1.html", title="Анкета", form=form,
+                               spec_link=url_for('static', filename="css/form_style.css"))
 
 
 if __name__ == '__main__':
