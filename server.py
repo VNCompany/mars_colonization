fb6fc730ffb007d7cf07b42ad506a8b8c1a613ba
victor <vneznanow@yandex.ru>
2020-03-25 17:28:11 +0300

    Тренировки в полёте

--- server.py
+++ server.py
@@ -10,6 +10,18 @@ def index(title_value):
     return render_template('base.html', title=title_value)
 
 
+@app.route("/training/<prof>")
+def training(prof: str):
+    prof_result = prof.lower()
+
+    if "инженер" in prof_result or "строитель" in prof_result:
+        prof_result = 1
+    else:
+        prof_result = 0
+
+    return render_template('training.html', title=prof, p=prof_result)
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
